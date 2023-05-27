24fa6439bdc448f5e1bb02bf3c82081fdf8ce47b
shivam chaudhary <shivamchaudhary837@gmail.com>
2023-05-27 18:32:01 +0530

    done changes

---
+++ online-shopping be/src/main/java/com/onlineshopping/dto/UserProfileRequest.java
@@ -0,0 +1,62 @@
+package com.onlineshopping.dto;
+
+import com.onlineshopping.model.Address;
+
+public class UserProfileRequest {
+
+	
+    private String firstName;
+    private String lastName;
+	
+	private String emailId;
+	
+	private String phoneNo;
+	private Address address;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public UserProfileRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
